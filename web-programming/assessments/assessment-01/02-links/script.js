cdc820312334fee5cb54affe2bffc26e34383bff
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-03-12 10:48:51 +0400

    ✨ Add info and character section

--- web-programming/assessments/assessment-01/02-links/script.js
+++ web-programming/assessments/assessment-01/02-links/script.js
@@ -1,5 +1,5 @@
 const headerLogo = document.querySelector("#logo");
 
 document.addEventListener("scroll", () => {
-  headerLogo.style.height = window.scrollY > 0 ?  "3rem" : "5rem"
+  headerLogo.style.fontSize = window.scrollY > 0 ?  "2rem" : "3rem"
 })
